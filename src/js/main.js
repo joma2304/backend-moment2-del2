bbd7c4ec8fc28402fdb1d075af4417d6a6aabd12
Johan Magnusson <joma2304@student.miun.se>
2024-04-11 12:36:50 +0200

    Lagt till för att kunna lägga till samt att sidan uppdateras automatiskt när man tar bort

--- src/js/main.js
+++ src/js/main.js
@@ -31,19 +31,55 @@ async function getWorkExperiences() { //Hämtar från api
   
   async function deleteWorkExperience(id) { //Funktion för att ta bort
     try {
-      const response = await fetch(`https://backend-moment2-del1.onrender.com/api/workexperience/${id}`, {
-        method: 'DELETE' //Delete metod för att ta bort från api och databas
-      });
-      if (!response.ok) {
-        throw new Error('Failed to delete work experience');
-      }
-      // Ta bort work experience från DOM
-      const listItem = document.querySelector(`li[data-id="${id}"]`);
-      listItem.remove();
+        const response = await fetch(`https://backend-moment2-del1.onrender.com/api/workexperience/${id}`, {
+            method: 'DELETE' //Delete metod för att ta bort från api och databas
+        });
+        if (!response.ok) {
+            throw new Error('Failed to delete work experience');
+        }
+        // Ladda om sidan för att uppdatera listan och därmed försvinner borttagna objektet
+        location.reload();
     } catch (error) {
-      console.error('Error:', error);
+        console.error('Error:', error);
     }
-  }
+}
+
   
   // Kör funktionen för att hämta och visa workexperience
   showWorkExperiences();
+
+  //Lägga till workexperience
+  document.getElementById('add-arbets-form').addEventListener('submit', addWorkExperience);
+
+async function addWorkExperience(event) {
+    event.preventDefault(); // Förhindrar att formuläret skickas som vanligt
+
+    const formData = new FormData(event.target); // Hämtar data från form
+
+    // Skapar ett objekt med den insamlade datan
+    const newWorkExperience = {
+        companyname: formData.get('companyname'),
+        jobtitle: formData.get('jobtitle'),
+        location: formData.get('location')
+        
+    };
+
+    try {
+        const response = await fetch('https://backend-moment2-del1.onrender.com/api/workexperience', {
+            method: 'POST', //Post metod
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newWorkExperience) // Omvandlar objektet till JSON
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to add work experience');
+        }
+
+        // Om det funkar skicka användaren till startsida så den kan se tillagda erfarenheten
+        window.location.href = 'index.html';
+    } catch (error) { //Om fel
+        console.error('Error:', error);
+    }
+}
