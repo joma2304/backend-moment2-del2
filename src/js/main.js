1c1a12e949a06b518feb07a6c36044ea86e9fb5d
Johan Magnusson <joma2304@student.miun.se>
2024-04-11 13:31:55 +0200

    Fixat menyn

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,5 @@
-
-
-// Hämta in meny-knapparna
-let openBtn = document.getElementById("open-menu");
-let closeBtn = document.getElementById("close-menu");
-
-//eventlyssnare
-openBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
-
-//Toggla fram navigeringsmenyn
-function toggleMenu() {
-    let navMenuEl = document.getElementById("nav-menu");
-
-    //hämtar in css för menyn
-    let style = window.getComputedStyle(navMenuEl);
-
-    //koll om navigering är synlig eller ej, ändrar display block/none
-    if(style.display === "none") {
-        navMenuEl.style.display = "block";
-    } else {
-        navMenuEl.style.display = "none";
-    }
-}
+//Väntar på att allt ska laddas in innan koden börjar köras
+document.addEventListener('DOMContentLoaded', function() {
 
 async function getWorkExperiences() { //Hämtar från api
     try {
@@ -108,3 +86,5 @@ async function addWorkExperience(event) {
         console.error('Error:', error);
     }
 }
+
+});
